0b697faab7bffc3180cdfc815ccb3206c133ed0d
Julio Arauz <jarauz@alumni.pitt.edu>
2024-09-12 11:39:20 -0400

    first commit

---
+++ v1/udpClient.py
@@ -0,0 +1,9 @@
+from socket import *
+serverIP = 'localhost'
+serverPort = 10000
+
+# Internet and UDP socket
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+message = input("Input lowercase sentence: ")
+clientSocket.sendto(message.encode(), (serverIP, serverPort))
---
+++ v1/udpServer.py
@@ -0,0 +1,12 @@
+from socket import *
+serverPort = 10000
+
+# Internet and UDP socket
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+f1=open("rxfile.txt", "w")
+serverSocket.bind(("localhost", serverPort)) 
+print ("Server socket ready... listening to port %d" % serverPort)
+while True:
+     data, addr = serverSocket.recvfrom(1024) # buffer size is 1024 bytes
+     print ("received message: %s" %data)
+     f1.write(data.decode("utf-8")+"\n")
